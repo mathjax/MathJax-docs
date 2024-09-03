e0a5033ef05bcebff67a3bd994f6b7a395c1b4dd
Davide P. Cervone <dpvc@union.edu>
2024-09-03 14:58:13 -0400

    Update configuration for ReadTheDocs

--- conf.py
+++ conf.py
@@ -13,6 +13,17 @@
 
 import sys, os
 
+### DPVC -- from https://about.readthedocs.com/blog/2024/07/addons-by-default/
+# Define the canonical URL if you are using a custom domain on Read the Docs
+html_baseurl = os.environ.get("READTHEDOCS_CANONICAL_URL", "")
+
+# Tell Jinja2 templates the build is running on Read the Docs
+if os.environ.get("READTHEDOCS", "") == "True":
+    if "html_context" not in globals():
+        html_context = {}
+    html_context["READTHEDOCS"] = True
+### /DPVC
+
 # If extensions (or modules to document with autodoc) are in another directory,
 # add these directories to sys.path here. If the directory is relative to the
 # documentation root, use os.path.abspath to make it absolute, like shown here.
